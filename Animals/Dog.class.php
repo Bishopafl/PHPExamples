79d957d49fd6139ccf9dd63eb07d2bf404d75d15
Adam Lopez <Adamf04@gmail.com>
2016-12-03 20:55:36 -0500

    added some changes to my dog class, however the more I look into it, I feel I might need to change some functions

--- Animals/Dog.class.php
+++ Animals/Dog.class.php
@@ -8,11 +8,12 @@ class Dog extends Animal{
   // Private class variables because these only apply to this class and we do not want them changed manually when the class is used
   private $isSleeping     // Bool
   private $isEating       // Bool
+  private $isInside       // Bool
   private $makingSound    // String
   private $numberOfFleas       // Int
   const DEFAULT_START_FLEAS = 0;
   const DEFAULT_ADD_FLEAS = 10;
-  const DEFAULT_KILLED_FLEAS = rand(0,100);
+  const DEFAULT_KILLED_FLEAS = rand(0,10);
 
   // Params for parent are optional since the parent class will set them if they are not sent in or are sent incorrectly
   // We will assume child class (Dog) has params that are sent in, will do some error handeling if they are not
@@ -38,11 +39,81 @@ class Dog extends Animal{
     if(gettype($isSleeping) == "boolean"){
       $this->isSleeping = $isSleeping;
     } else {
-      throw new Exception("If the ")
+      throw new Exception("The dog is either sleeping or it isn't, type of " .gettype($isSleeping) . " was sent in.");
     }
   }
 
+  private function setIsEating($isEating){
+    if(gettype($isEating) == "boolean"){
+      $this->isEating = $isEating;
+    } else {
+      throw new Exception("The dog is either eating or it isn't, type of " . gettype($isEating) . " was sent in.");
+    }
+  }
 
+  private function setIsInside($isInside){
+    if(gettype($isInside) == "boolean"){
+      $this->isInside = $isInside;
+    } else {
+      throw new Exception("The dog is either inside or isn't, type of " . gettype($makingSound) . " was sent in.");
+    }
+  }
+
+  private function setIsMakingSound($makingSound){
+    if(gettype($makingSound) == "string"){
+      $this->isMakingSound = $makingSound;
+    } else {
+      throw new Exception("Sound must be a string, type of " . gettype($makingSound) . " was sent in.");
+    }
+  }
+
+  private function setNumberOfFleas($numberOfFleas){
+    if(gettype($numberOfFleas) == "integer"){
+      $this->numberOfFleas = $numberOfFleas;
+    } else {
+      throw new Excepetion("The number of fleas must be integer, type of " . getype($numberOfFleas) . " was sent in.");
+    }
+  }
+
+  // Getters are all public becauae we dont care if anyone else sees them
+  public function getIsSleeping(){
+    return $this->isSleeping;
+  }
+
+  public function getIsEating(){
+    return $this->isEating;
+  }
+
+  public function getIsMakingSound(){
+    return $this->isMakingSound;
+  }
+
+  public function getIsInside(){
+    return $this->isInside;
+  }
+
+  public function getNumberOfFleas(){
+    return $this->numberOfFleas;
+  }
+
+  // End of setters and getters
+
+  // Okay now for some fun class-specific methods that we can use in our code
+
+  // When the dog is outside, it will have more fleas, this method should only be accessible by this class and is private
+  // This is private because we only want tohave this function called inside this class
+
+  private function goingOutside(){
+    $this->setNumberOfFleas(($this->getNumberOfFleas() + self::DEFAULT_START_FLEAS));
+    $this->setNumberOfFleas(true);
+  }
+
+  private function fleaCheck(){
+    $this->setHasFleas($this->getNumberOfFleas() + self::DEFAULT_ADD_FLEAS);
+    $this->setHasFleas(true);
+  }
+
+  private function
 
 }
 
